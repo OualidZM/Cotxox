2ffb9d648e5a6e35418d6ab315bb81f49737c429
ozaaj <ozaaj@cifpfbmoll.eu>
2021-04-09 19:31:22 +0200

    docs: :construction: Tarifa.java
    we create the base of the class Tarifa.java

--- Cotxox/Cotxox/src/main/java/edu/pingpong/Cotxox/tarifa/Tarifa.java
+++ Cotxox/Cotxox/src/main/java/edu/pingpong/Cotxox/tarifa/Tarifa.java
@@ -1,5 +1,27 @@
 package edu.pingpong.Cotxox.tarifa;
 
+import edu.pingpong.Cotxox.carrera.Carrera;
+
 public class Tarifa {
-    
+    static  final double COSTE_MILLA = 1.35;
+    static  final double COSTE_MINUTO = 0.35;
+    static  final double COSTE_MINIMO = 5;
+    static  final byte PORCENTAJE_COMISION = 20;
+
+    Tarifa(){}
+
+    static double  getCosteDistancia(double distancia){
+
+    }
+
+    static double getCosteTiempo(int minutos){
+
+    }
+    static double getCosteTotalEsperado(Carrera carrera){
+
+    }
+
+
 }
+
+
