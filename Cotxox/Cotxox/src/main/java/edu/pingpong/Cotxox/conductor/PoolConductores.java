9bc132b9e6f3e3e851899efe5e18805ea2fa1649
ozaaj <ozaaj@cifpfbmoll.eu>
2021-04-09 20:00:43 +0200

    docs: :construction: PoolConductores.java
    Creating base of the class PoolConductores.java

--- Cotxox/Cotxox/src/main/java/edu/pingpong/Cotxox/conductor/PoolConductores.java
+++ Cotxox/Cotxox/src/main/java/edu/pingpong/Cotxox/conductor/PoolConductores.java
@@ -1,5 +1,21 @@
 package edu.pingpong.Cotxox.conductor;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PoolConductores {
     
+    private List<Conductor> conductors= new ArrayList<Conductor>();
+    
+
+    public PoolConductores(List<Conductor> conductors) {
+        this.conductors = conductors;
+    }
+
+    public List<Conductor> getPoolConductores(){
+    }
+
+    public Conductor asignarConductor(){
+        
+    }
 }
