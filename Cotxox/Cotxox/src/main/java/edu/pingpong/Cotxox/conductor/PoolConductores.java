7a4edacda4cd9a0403367546047c80f59565e5d1
ozaaj <ozaaj@cifpfbmoll.eu>
2021-04-10 00:39:42 +0200

    docs: :construction: PoolConductores.java

--- Cotxox/Cotxox/src/main/java/edu/pingpong/Cotxox/conductor/PoolConductores.java
+++ Cotxox/Cotxox/src/main/java/edu/pingpong/Cotxox/conductor/PoolConductores.java
@@ -2,20 +2,39 @@ package edu.pingpong.Cotxox.conductor;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class PoolConductores {
     
-    private List<Conductor> conductors= new ArrayList<Conductor>();
     
 
-    public PoolConductores(List<Conductor> conductors) {
-        this.conductors = conductors;
+    private List<Conductor> poolConductores = new ArrayList<Conductor>();
+
+    public PoolConductores(List<Conductor> poolConductores) {
+        this.poolConductores = poolConductores;
     }
 
     public List<Conductor> getPoolConductores(){
+        return this.poolConductores;
     }
 
     public Conductor asignarConductor(){
-        
+        Conductor conductor = new Conductor();
+        Random random = new Random();
+        boolean asignado = false;
+
+        while(!asignado){
+            int position = random.nextInt(getPoolConductores().size());
+            conductor = getPoolConductores().get(position);
+
+            if(!conductor.isOcupado()){
+                conductor.setOcupado(true);
+                asignado = true;
+            }
+
+        }
+
+        return conductor;
+    
     }
 }
