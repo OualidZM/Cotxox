0f0cf83d1377aa319de50b5b460002b7efd64951
ozaaj <ozaaj@cifpfbmoll.eu>
2021-04-09 20:30:49 +0200

    docs: :construction: Carrera.java
    Creating the attributes of the class Carrera.java

--- Cotxox/Cotxox/src/main/java/edu/pingpong/Cotxox/carrera/Carrera.java
+++ Cotxox/Cotxox/src/main/java/edu/pingpong/Cotxox/carrera/Carrera.java
@@ -2,4 +2,14 @@ package edu.pingpong.Cotxox.carrera;
 
 public class Carrera {
     
+    private String tarjetaCredito = null;
+    private String origen = null;
+    private String destino = null;
+    private String distancia = null;
+    private int tiempoEsperado = 0;
+    private int tiempoCarrera = 0;
+    private double costeTotal = 0;
+    private int propina = 0;
+    
+
 }
