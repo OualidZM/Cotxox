2e1fd4b236f5733df25fb0897e1679fbce75e448
ozaaj <ozaaj@cifpfbmoll.eu>
2021-04-09 20:21:06 +0200

    docs: :construction: Conductor.java
    Creating the base of Conductor.java

--- Cotxox/Cotxox/src/main/java/edu/pingpong/Cotxox/conductor/Conductor.java
+++ Cotxox/Cotxox/src/main/java/edu/pingpong/Cotxox/conductor/Conductor.java
@@ -1,5 +1,68 @@
 package edu.pingpong.Cotxox.conductor;
 
+import java.util.ArrayList;
+
 public class Conductor {
+
+    private String nombre = null;
+    private String modelo = null;
+    private String matricula = null;
+    private double valoracionMedia = 0;
+    private boolean ocupado = false;
+    private ArrayList<Byte> valoraciones;
+
+    public Conductor(){
+
+    }
+
+    public Conductor(String nombre){
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+
+    public String getMatricula() {
+        return matricula;
+    }
     
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public double getValoracion() {
+        return valoracionMedia;
+    }
+
+    public int getNumeroValoracion(){
+
+    }
+
+
+    public void setValoracion(byte valoracion){
+
+    }
+
+    private double calcularValoracionMedia(){
+
+    }
+
+    public void setOcupado(boolean ocupado) {
+        this.ocupado = ocupado;
+    }
+
+    public boolean isOcupado() {
+        return ocupado;
+    }
 }
