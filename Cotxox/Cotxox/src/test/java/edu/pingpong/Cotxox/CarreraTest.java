0b8693a28937e5a21ab25d4c6cff80d9aacee390
ozaaj <ozaaj@cifpfbmoll.eu>
2021-04-10 00:44:23 +0200

    docs: :construction: TDD
    In the folder Test I created the classes that are going to test each part of the program, where I will make the test for the program

---
+++ Cotxox/Cotxox/src/test/java/edu/pingpong/Cotxox/CarreraTest.java
@@ -0,0 +1,16 @@
+package edu.pingpong.Cotxox;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+
+public class CarreraTest 
+{
+
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
---
+++ Cotxox/Cotxox/src/test/java/edu/pingpong/Cotxox/TarifaTest.java
@@ -0,0 +1,15 @@
+package edu.pingpong.Cotxox;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class TarifaTest 
+{
+
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
