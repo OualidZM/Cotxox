52d8265ef03b672e667d09a3e64ea91d5cbdabc5
ozaaj <ozaaj@cifpfbmoll.eu>
2021-04-10 11:22:50 +0200

    docs: :construction: ConductorTest.java
    Making the base to test the class ConductorTest.java

--- Cotxox/Cotxox/src/test/java/edu/pingpong/Cotxox/ConductorTest.java
+++ Cotxox/Cotxox/src/test/java/edu/pingpong/Cotxox/ConductorTest.java
@@ -1,16 +1,62 @@
 package edu.pingpong.Cotxox;
 
 import static org.junit.Assert.assertTrue;
-
+import org.junit.Before;
 import org.junit.Test;
+import edu.pingpong.Cotxox.conductor.Conductor;
 
 
 public class ConductorTest 
 {
 
+    Conductor conductor;
+    
+    @Before
+    public void before(){
+        conductor = new Conductor("Antonio");
+        conductor = new Conductor("Lucas");
+        conductor = new Conductor("Daniel");
+        conductor = new Conductor("Paula");
+        conductor = new Conductor("Pablo");
+
+    }
+
+    
     @Test
-    public void shouldAnswerWithTrue()
+    public void setModeloTest()
     {
         assertTrue( true );
     }
+
+
+    @Test
+    public void getMatriculaTest()
+    {
+        assertTrue( true );
+    }
+
+    @Test
+    public void getValoracionTest()
+    {
+        assertTrue( true );
+    } 
+    
+    @Test
+    public void getNumeroValoracionTest()
+    {
+        assertTrue( true );
+    }
+
+    @Test
+    public void getOcupadoTest()
+    {
+        assertTrue( true );
+    }
+
+    @Test
+    public void isOcupadoTest()
+    {
+        assertTrue( true );
+    }
+
 }
