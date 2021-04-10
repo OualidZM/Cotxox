eea61be3ad6f74b1c3e0e0334a32fb1d17293c15
ozaaj <ozaaj@cifpfbmoll.eu>
2021-04-10 02:48:57 +0200

    docs: :art: Tarifa.java
    Making the methods public

--- Cotxox/Cotxox/src/main/java/edu/pingpong/Cotxox/tarifa/Tarifa.java
+++ Cotxox/Cotxox/src/main/java/edu/pingpong/Cotxox/tarifa/Tarifa.java
@@ -8,17 +8,16 @@ public class Tarifa {
     static  final double COSTE_MINIMO = 5;
     static  final byte PORCENTAJE_COMISION = 20;
 
-    Tarifa(){}
 
-    static double  getCosteDistancia(double distancia){
+    public static double  getCosteDistancia(double distancia){
         return distancia * COSTE_MILLA;
     }
 
-    static double getCosteTiempo(int minutos){
+    public static double getCosteTiempo(int minutos){
         return minutos * COSTE_MINUTO;
          
     }
-    static double getCosteTotalEsperado(Carrera carrera){
+    public static double getCosteTotalEsperado(Carrera carrera){
         double costeRecorrido = getCosteDistancia(carrera.getDistancia()) + getCosteTiempo(carrera.getTiempoEsperado());
         double comision = (costeRecorrido * PORCENTAJE_COMISION) /100;
         double total = costeRecorrido + comision;
