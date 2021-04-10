432a6d43123ace5da5f0d7fc52e54ba22e10f052
ozaaj <ozaaj@cifpfbmoll.eu>
2021-04-10 13:31:41 +0200

    docs: :art: PoolConductoresTest.java

--- Cotxox/Cotxox/src/test/java/edu/pingpong/Cotxox/PoolConductoresTest.java
+++ Cotxox/Cotxox/src/test/java/edu/pingpong/Cotxox/PoolConductoresTest.java
@@ -1,16 +1,29 @@
 package edu.pingpong.Cotxox;
 
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
+
+import edu.pingpong.Cotxox.conductor.Conductor;
+import edu.pingpong.Cotxox.conductor.PoolConductores;
 
 
 public class PoolConductoresTest 
 {
 
+    List<Conductor> poolConductores = new ArrayList<Conductor>();
+    Conductor conductor;
+    PoolConductores poolConductoress;
+
     @Test
-    public void shouldAnswerWithTrue()
+    public void Constructor()
     {
-        assertTrue( true );
+        assertNull(poolConductoress);
     }
+
 }
